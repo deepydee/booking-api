0b38b9185066dd8ccb3f78fa34d9ab54ef90a939
Vitalii Semenov <deepy_dee@bk.ru>
2024-05-22 09:06:27 +0500

    chore: config modular

---
+++ app/stubs/modular/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * StubModuleName module translation file.
+ * All language files return an array of keyed strings.
+ *
+ * @see https://laravel.com/docs/10.x/localization#defining-translation-strings
+ */
+return [
+    // 'key_to_translate' => 'Здесь перевод на русский язык',
+];
